1bf1e81e8868a7ce76a04a4f1b9b012b769bbfd6
AlexandrKhom <alex988andr@gmail.com>
2021-07-26 01:11:42 +0300

    al2

--- src/App.js
+++ src/App.js
@@ -1,46 +1,78 @@
-import './App.css';
 import React, {useEffect, useState} from "react";
+import {BrowserRouter as Router, Route, Link, Switch, useParams, useHistory} from "react-router-dom";
 
+export default function App() {
+    return (<Router>
+        <div>
+            <h2><Link to={'/'}>Home</Link></h2>
+            <h2><Link to={'/about'}>about</Link></h2>
+            <h2><Link to={'/users'}>users</Link></h2>
+            <h2><Link to={'/test'}>test</Link></h2>
 
-function App() {
-
-    const [point, setPoint] = useState('')
-    const [id, setId] = useState('')
-    const [one, setOne] = useState(null)
-    const [more, setMore] = useState([])
-
-    const url = `https://jsonplaceholder.typicode.com/`
-
-    const dataFetch = async () => {
-        const resp = await fetch(`${url}${point}/${id}`)
-        const data = await resp.json()
-        if (id) {
-            setOne(data)
-            setMore([])
-            return
-        }
-        setMore(data)
-        setOne(null)
-    }
-
-    // useEffect(() => {
-    //     dataFetch()
-    // }, [point])
-
-    return (
-        <div className={'App'}>
-            <input type={'text'} name={'point'} placeholder={'point'}
-                   value={point} onChange={(event => setPoint(event.target.value))}
-            />
-            <input type={'number'} name={'id'} placeholder={'id'}
-                   value={id} onChange={({target: {value}}) => setId(value)}
-            />
-            <button onClick={dataFetch}>click</button>
-            <hr/>
-            {one && (<h2>{JSON.stringify(one, null, 2)}</h2>)}
-            {more.map(el=> (<h2>{el.id}-{el.name ?? el.title}</h2>))}
+            <Switch>
+                <Route path={'/'} exact><Home/></Route>
+                <Route path={'/about'}>{About}</Route>
+                <Route path={'/users'} exact component={Users}/>
+                <Route path={'/test'} render={(arg)=> {
+                    console.log(arg)
+                    return <Test/>
+                }}
+                />
+                <Route path={'/users/:id'}><Detal/></Route>
+            </Switch>
         </div>
-    );
+    </Router>);
 }
 
-export default App;
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function About(props) {
+    console.log(props)
+    return <h2>About</h2>;
+}
+
+function Users(props) {
+    console.log(props)
+    const [users, setUsers] = useState([])
+
+    const dataFetch = async ()=> {
+        const resp = await fetch(`https://jsonplaceholder.typicode.com/users`)
+        const data = await resp.json()
+        setUsers(data)
+    }
+    useEffect(()=> {dataFetch()}, [])
+
+    return (<div>
+        {users.map(el => (<Link key={el.id} to={`/users/${el.id}`}><h2 key={el.id}>{el.id}-{el.name}</h2></Link>))}
+    </div>)
+}
+
+function Test() {
+    return <h2>TEST</h2>
+}
+
+function Detal() {
+    const [user, setUser] = useState();
+    const {id} = useParams();
+    const history = useHistory();
+
+    const fetchData = async () => {
+        const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+        const data = await resp.json()
+        setUser(data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [id])
+
+    return (
+        <div>
+            <h1>Detal</h1>
+            {user && (<h2>{user.id} - {user.name}</h2>)}
+            <button onClick={() => history.push(`/users/${+id + 1}`)}>next</button>
+        </div>
+    )
+}
